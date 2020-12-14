e66107504333033b627ccfc30c0c89fbadf5e299
Saif-Ihsan <ddsaifihsan@gmail.com>
2020-12-14 13:30:49 +0200

    add theme& tailwind & firebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
   parserOptions: {
@@ -12,6 +12,73 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'curly': ['error', 'all'],
+    'eqeqeq': ['error', 'always'],
+    'no-loop-func': ['error'],
+    'no-native-reassign': ['error'],
+    'no-self-compare': ['error'],
+    'yoda': ['error', 'never'],
+    'no-unused-vars': ['warn'],
+    'no-restricted-imports': ['error', '*'],
+    'vue/order-in-components': ['error', {
+      'order': [
+        'el',
+        'name',
+        'key',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        ['provide', 'inject'],
+        'ROUTER_GUARDS',
+        'layout',
+        'middleware',
+        'validate',
+        'scrollToTop',
+        'transition',
+        'loading',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'emits',
+        'setup',
+        'asyncData',
+        'data',
+        'fetch',
+        'head',
+        'computed',
+        'watch',
+        'watchQuery',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }],
+    'vue/custom-event-name-casing': ['error', {
+      'ignores': []
+    }],
+    'vue/v-bind-style': ['error', 'shorthand'],
+    'vue/v-on-style': ['error', 'shorthand'],
+    'vue/no-mutating-props': ['off'],
+    'vue/no-parsing-error': ['off', {
+      'invalid-first-character-of-tag-name': true,
+    }]
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
