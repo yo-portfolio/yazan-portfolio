0f7cacaa44ed6bce1caf0e0c2cd4b8c0e5043fb1
Saif-Ihsan <ddsaifihsan@gmail.com>
2021-01-13 19:19:55 +0200

    fix the router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <div class="hidden hidden-ball scale-title">
+    </div> -->
+        <router-view/>
+    <div class="hidden hidden-ball scale-title" style="display: none !important;">
 
-    <main>		
+    <main >		
           <!-- Preloader -->
           <div class="preloader-wrap">
               <div class="outer">
