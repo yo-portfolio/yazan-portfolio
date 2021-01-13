eea03767a142ada00d274abf9e45e40beb0c0612
Saif-Ihsan <ddsaifihsan@gmail.com>
2021-01-13 19:20:15 +0200

    setep firebase

---
+++ src/firebaseDb.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCyBsehgigN7i5Ur15rKDxWScm7iaSpN3k",
+    authDomain: "yazan-portfolio.firebaseapp.com",
+    databaseURL: "https://yazan-portfolio.firebaseio.com",
+    projectId: "yazan-portfolio",
+    storageBucket: "yazan-portfolio.appspot.com",
+    messagingSenderId: "609539689356",
+    appId: "1:609539689356:web:d6d2c8264eb18135b164a9",
+    measurementId: "G-GBJ4BGP25G"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '../tailwind.css';
+import  db  from './firebaseDb.js';
+
+
 Vue.config.productionTip = false
 
 new Vue({
